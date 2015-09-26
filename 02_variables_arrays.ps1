7865588387fb59174b9e5358a56109b250936b06
Daniel Flint <nauzilus@gmail.com>
2015-09-26 19:38:33 +1000

    Demonstrate array concatenation without flattening

--- 02_variables_arrays.ps1
+++ 02_variables_arrays.ps1
@@ -1,12 +1,19 @@
 ﻿$names = @("Lee", "Kim", "Jamie")
-Write-Host "names => $([string]::Join(', ', $names))"
+Write-Host "names => $($names -join ', ')"
+
 $names += "Ainsley"
 Write-Host "names => $($names -join ', ')"
 
 $numbers = @(0 .. 9)
-Write-Host "numbers => $($numbers -join ', '))"
+Write-Host "numbers => $($numbers -join ', ')"
+
 $alphas = "abcdef".ToCharArray() # a..f doesn't work =(
 Write-Host "alphas => $($alphas -join ', ')"
 
+# arrays are flattened when concatenating
 $hex = $numbers + $alphas
+Write-Host "hex => $($hex -join ', ')"
+
+# array of arrays
+$hex = ,$numbers + ,$alphas
 Write-Host "hex => $($hex -join ', ')"
